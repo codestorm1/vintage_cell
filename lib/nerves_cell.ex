8c40361d71d571a41ad9630743a38e3184516638
Bryan Green <twistedogre@gmail.com>
2023-10-31 22:44:24 -0700

    first commit

---
+++ lib/nerves_cell.ex
@@ -0,0 +1,18 @@
+defmodule NervesCell do
+  @moduledoc """
+  Documentation for NervesCell.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> NervesCell.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/nerves_cell_test.exs
@@ -0,0 +1,8 @@
+defmodule NervesCellTest do
+  use ExUnit.Case
+  doctest NervesCell
+
+  test "greets the world" do
+    assert NervesCell.hello() == :world
+  end
+end
