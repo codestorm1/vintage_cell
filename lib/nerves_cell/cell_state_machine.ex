6322c41c8210c653e4debe5448511c9a55979a42
Bryan Green <twistedogre@gmail.com>
2023-12-07 01:29:02 -0800

    Starting to experiment with GenStateMachine

--- lib/nerves_cell/cell_state_machine.ex
+++ lib/nerves_cell/cell_state_machine.ex
@@ -16,14 +16,47 @@ defmodule CellStateMachine do
   def on({:call, from}, :get_count, data) do
     {:keep_state_and_data, [{:reply, from, data}]}
   end
+
+  # def onhook({:cast})
+  def onhook(:cast, :flip, 0) do
+    nil
+  end
 end
 
 # Start the server
-{:ok, pid} = GenStateMachine.start_link(CellStateMachine, {:off, 0})
+{:ok, pid} = GenStateMachine.start_link(CellStateMachine, {:onhook, 0})
 
 # This is the client
 GenStateMachine.cast(pid, :flip)
 # => :ok
 
-GenStateMachine.call(pid, :get_count)
-# => 1
+# GenStateMachine.call(pid, :get_count)
+# # => 1
+
+# defmodule Switch do
+#   use GenStateMachine, callback_mode: :state_functions
+
+#   def off(:cast, :flip, data) do
+#     {:next_state, :on, data + 1}
+#   end
+#   def off({:call, from}, :get_count, data) do
+#     {:keep_state_and_data, [{:reply, from, data}]}
+#   end
+
+#   def on(:cast, :flip, data) do
+#     {:next_state, :off, data}
+#   end
+#   def on({:call, from}, :get_count, data) do
+#     {:keep_state_and_data, [{:reply, from, data}]}
+#   end
+# end
+
+# # Start the server
+# {:ok, pid} = GenStateMachine.start_link(Switch, {:off, 0})
+
+# # This is the client
+# GenStateMachine.cast(pid, :flip)
+# #=> :ok
+
+# GenStateMachine.call(pid, :get_count)
+# #=> 1
