72b8170eab4436851ad8337b7582d2cd4efbe9ce
Bryan Green <twistedogre@gmail.com>
2024-03-03 02:44:00 -0800

    Switching over to using rotary dialer at this stage.  Removed KeypadDialer and Keypad dependency

--- mix.exs
+++ mix.exs
@@ -70,7 +70,6 @@ defmodule NervesCell.MixProject do
       {:nerves_system_x86_64, "~> 1.24", runtime: false, targets: :x86_64},
       {:nerves_system_grisp2, "~> 0.8", runtime: false, targets: :grisp2},
       {:nerves_system_mangopi_mq_pro, "~> 0.6", runtime: false, targets: :mangopi_mq_pro},
-      {:keypad, path: "../keypad"},
       # {:fona_modem, git: "git@github.com:codestorm1/fona_modem.git", tag: "v0.1.0"}
       {:fona_modem, path: "../fona_modem"},
       {:gen_state_machine, "~> 3.0"}
